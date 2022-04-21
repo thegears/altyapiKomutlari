ac162a77112f1ae7a9df852c2247f7c551157ba7
thegears <65253167+thegears@users.noreply.github.com>
2022-04-21 22:49:25 +0300

    Update deprem-bilgi.js

--- deprem-bilgi.js
+++ deprem-bilgi.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const axios = require("axios");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
     name : "deprem-bilgi",
@@ -10,11 +11,11 @@ module.exports = {
         a = a.data.result;
 
         a.forEach((d,i) => {
-          embeddescription += `${i + 1}. ${d.lokasyon} | ${d.depth}\n`;
+          embeddescription += `${i + 1}. ${d.lokasyon} | ${d.mag}\n`;
         });
         
         await int.reply({
-          embeds : [ new MessageEmbed().setDescription(embeddescription).setColor("RANDOM") ],
+          embeds : [ new MessageEmbed().setDescription(embeddescription).setColor("RANDOM").setThumbnail("https://imgs.search.brave.com/N3uR0KiofI9Gvbhy4B7_6bTB7En9wAjx4kTWldnRp4A/rs:fit:817:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5i/MmYzY3p4b3k1bkRq/bzlxaHRZb1F3SGFF/VCZwaWQ9QXBp").setTitle("Yaşanan son depremler").setTimestamp() ],
 ephemeral : true
         });  
 
